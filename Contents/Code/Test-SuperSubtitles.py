e241e98c8c663dc4af0ef82c863106da766e6b66
Bálint Böde <balint.bode@gmail.com>
2014-07-20 12:29:51 +0200

    Change zip file handling

--- Contents/Code/Test-SuperSubtitles.py
+++ Contents/Code/Test-SuperSubtitles.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 import SuperSubtitlesSearch
 
 class MyTestCase(unittest.TestCase):
@@ -10,6 +11,15 @@ class MyTestCase(unittest.TestCase):
     def test_movie(self):
         id = SuperSubtitlesSearch.search_show("Leaving las vegas", 'tt0113627')
         print(id)
+
+    def test_regexp(self):
+        regexp = re.compile(r'((\d+)x(\d+)|s(\d+)e(\d+))', re.I)
+        match = regexp.search('The Strain - 01x01 - Night Zero.DIMENSION.Eng.srt')
+        if match:
+            print(match.groups())
+        match = regexp.search('The.Strain.S01E01.HDTV.XviD-FUM.srt')
+        if match:
+            print(match.groups())
 
 
 if __name__ == '__main__':
--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -67,35 +67,41 @@ class SuperSubtitleAgentTv(Agent.TV_Shows):
     primary_provider = False
     contributes_to = ['com.plexapp.agents.thetvdb']
 
+    episode_pattern = re.compile(r'(\d+)x(\d+)|s(\d+)e(\d+)', re.I)
+
     def search(self, results, media, lang, manual):
         show_id = SuperSubtitlesSearch.search_show(media.primary_metadata.title, media.primary_metadata.id)
         if show_id is not None:
             results.Append(MetadataSearchResult(id=show_id, score=100))
 
     def update(self, metadata, media, lang):
-        name_pattern = re.compile(r'^.* - (\d*)x(\d*) - .* \((.*)\).*')
         for s in media.seasons:
             for e in media.seasons[s].episodes:
                 for item in media.seasons[s].episodes[e].items:
                     for part in item.parts:
                         for lang in (Prefs['lang1'], Prefs['lang2']):
-                            if lang == 'None':
-                                continue
-                            results = SuperSubtitlesSearch.get_tv_subtitles(metadata.id, lang, s, e)
-                            result = SuperSubtitlesSearch.filter_subtitles(results, part.file)
-                            if not result:
-                                continue
-                            subtitle = SuperSubtitlesSearch.download_subtitle(result.id)
-                            extension = subtitle[0].split('.')[-1]
-                            if extension == 'zip':
-                                zip_archive = Archive.Zip(subtitle[1])
-                                for name in zip_archive:
-                                    if name[-1] == '/':
-                                        continue
-                                    match = name_pattern.match(str(name))
-                                    extension = str(name).split('.')[-1]
-                                    if match and int(match.group(1)) == int(s) and int(match.group(2)) == int(e):
-                                        part.subtitles[languages[lang]][result.id] = Proxy.Media(zip_archive[name], ext=extension)
-                                        break
-                            else:
-                                part.subtitles[languages[lang]][result.id] = Proxy.Media(subtitle[1], ext=extension)
+                            self.update_part(lang, metadata, media, s, e, part)
+
+    def update_part(self, language, metadata, media, season, episode, part):
+        if language == 'None':
+            return
+        results = SuperSubtitlesSearch.get_tv_subtitles(metadata.id, language, season, episode)
+        result = SuperSubtitlesSearch.filter_subtitles(results, part.file)
+        if not result:
+            return
+        subtitle = SuperSubtitlesSearch.download_subtitle(result.id)
+        extension = subtitle[0].split('.')[-1]
+        if extension == 'zip':
+            zip_archive = Archive.Zip(subtitle[1])
+            for name in zip_archive:
+                if name[-1] == '/':
+                    continue
+                extension = str(name).split('.')[-1]
+                match = self.episode_pattern.search(str(name))
+                hit = match and match.group(1) is not None and int(match.group(1)) == int(season) and int(match.group(2)) == int(episode)
+                hit = hit if hit else match and match.group(3) is not None and int(match.group(3)) == int(season) and int(match.group(4)) == int(episode)
+                if hit:
+                    part.subtitles[languages[language]][result.id] = Proxy.Media(zip_archive[name], ext=extension)
+                    break
+        else:
+            part.subtitles[languages[language]][result.id] = Proxy.Media(subtitle[1], ext=extension)
